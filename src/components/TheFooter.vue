7882b2017d628b9b3c6910f1c22980bd6add8aac
mahosobe <mariia.honchariuk@sobaco-betax.com>
2024-04-15 15:00:09 +0200

    Footer, Contact

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -15,14 +15,14 @@
         <div class="block p-4 w-100 ml-16">
           <div class="flex justify-between">
             <div class="links">
-              <router-link class="border-l-2 border-my-yellow my-2 ml-16 pl-2" to="">Home</router-link>
-              <router-link class="border-l-2 border-my-yellow my-2 ml-16 pl-2" to="">Home</router-link>
-              <router-link class="border-l-2 border-my-yellow my-2 ml-16 pl-2" to="">Home</router-link>
-              <router-link class="border-l-2 border-my-yellow my-2 ml-16 pl-2" to="">Home</router-link>
-              <router-link class="border-l-2 border-my-yellow my-2 ml-16 pl-2" to="">Home</router-link>
-              <router-link class="border-l-2 border-my-yellow my-2 ml-16 pl-2" to="">Home</router-link>
+              <router-link class="border-l-2 border-my-yellow my-2 ml-16 pl-2" :to="{ name: 'TheNews'}">{{$t('health')}}</router-link>
+              <router-link class="border-l-2 border-my-yellow my-2 ml-16 pl-2" :to="{ name: 'TheContact'}">{{ $t('contact') }}</router-link>
+              <router-link class="border-l-2 border-my-yellow my-2 ml-16 pl-2" :to="{ name: 'TrashSeparation'}">{{ $t('trash-separation') }}</router-link>
+              <router-link class="border-l-2 border-my-yellow my-2 ml-16 pl-2" :to="{ name: 'RecyclingCycle'}">{{ $t('recycling-cycle') }}</router-link>
+              <router-link class="border-l-2 border-my-yellow my-2 ml-16 pl-2" :to="{ name: 'EnvironmentalProtection'}">{{ $t('environmental-protection') }}</router-link>
+              <router-link class="border-l-2 border-my-yellow my-2 ml-16 pl-2" :to="{ name: 'TheHome'}">{{ $t('home') }}</router-link>
             </div>
-            <router-link to="" class="bg-my-yellow p-4 rounded-3xl px-7 text-black h-14">{{ $t('contact-us.title') }}</router-link>
+            <router-link to="" class="bg-my-yellow p-4 rounded px-7 text-black h-14">{{ $t('contact-us.title') }}</router-link>
           </div>
           <div class="ml-16 border-t-2 border-my-yellow flex text-white justify-end">
             <svg xmlns="http://www.w3.org/2000/svg" class="ml-6 mt-4" width="20" viewBox="0 0 448 512"><path fill="#fff61a" d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z"/></svg>
--- src/pages/TheContact.vue
+++ src/pages/TheContact.vue
@@ -41,24 +41,16 @@ export default {
 </script>
 
 <template>
-  <div class="contact flex justify-around pt-24 mx-auto" style="height: 80vh; ">
-    <div class="info absolute bg-black p-12 top-1/4 " style="right: 18%;">
-      <div class="absolute w-10 bg-my-yellow h-10" style="top: -5%; left: -5%;"></div>
-      <h1 class="font-bold text-4xl py-6">{{ $t('contact-us.info') }}</h1>
-      <div class="flex py-4 w-96 align-center">
-        <svg width="30" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="#058240" d="M48 64C21.5 64 0 85.5 0 112c0 15.1 7.1 29.3 19.2 38.4L236.8 313.6c11.4 8.5 27 8.5 38.4 0L492.8 150.4c12.1-9.1 19.2-23.3 19.2-38.4c0-26.5-21.5-48-48-48H48zM0 176V384c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V176L294.4 339.2c-22.8 17.1-54 17.1-76.8 0L0 176z"/></svg>
-        <a href="mailto:maria.spam2002@gmail.com" class="ml-6">maria.spam2002@gmail.com</a>
-      </div>
-      <div class="flex py-4 align-center">
-        <svg width="30" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="#058240" d="M164.9 24.6c-7.7-18.6-28-28.5-47.4-23.2l-88 24C12.1 30.2 0 46 0 64C0 311.4 200.6 512 448 512c18 0 33.8-12.1 38.6-29.5l24-88c5.3-19.4-4.6-39.7-23.2-47.4l-96-40c-16.3-6.8-35.2-2.1-46.3 11.6L304.7 368C234.3 334.7 177.3 277.7 144 207.3L193.3 167c13.7-11.2 18.4-30 11.6-46.3l-40-96z"/></svg>
-        <p class="ml-6">+49 160 24 171 36</p>
-      </div>
-      <div class="flex pt-4 pb-6 align-center">
-        <svg width="30" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512"><path fill="#058240" d="M288 48c0-26.5 21.5-48 48-48h96c26.5 0 48 21.5 48 48V192h40V120c0-13.3 10.7-24 24-24s24 10.7 24 24v72h24c26.5 0 48 21.5 48 48V464c0 26.5-21.5 48-48 48H432 336c-26.5 0-48-21.5-48-48V48zm64 32v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V80c0-8.8-7.2-16-16-16H368c-8.8 0-16 7.2-16 16zm16 80c-8.8 0-16 7.2-16 16v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V176c0-8.8-7.2-16-16-16H368zM352 272v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V272c0-8.8-7.2-16-16-16H368c-8.8 0-16 7.2-16 16zm176-16c-8.8 0-16 7.2-16 16v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V272c0-8.8-7.2-16-16-16H528zM512 368v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V368c0-8.8-7.2-16-16-16H528c-8.8 0-16 7.2-16 16zM224 160c0 6-1 11-2 16c20 14 34 38 34 64c0 45-36 80-80 80H160V480c0 18-15 32-32 32c-18 0-32-14-32-32V320H80c-45 0-80-35-80-80c0-26 13-50 33-64c-1-5-1-10-1-16c0-53 42-96 96-96c53 0 96 43 96 96z"/></svg>
-        <a href="https://www.google.com/maps/place/Amberger+Str.+180,+93057+Regensburg/@49.048593,12.0988491,17z/data=!3m1!4b1!4m5!3m4!1s0x479fc1d15f1383f7:0xb5e53f3d9485f9d7!8m2!3d49.0485895!4d12.101424?entry=ttu" class="ml-6">Ambereger-Str. 180, 93057 Regensburg, Germany</a>
-      </div>
+  <div class="contact flex justify-between pt-12" style="height: 100vh; ">
+    <div class="images" style="width: 52.5%;">
+      <img src="https://images.pexels.com/photos/802221/pexels-photo-802221.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1">
+      <img src="https://cdn.pixabay.com/photo/2017/08/16/10/54/waste-2647287_640.jpg">
+      <img src="https://images.pexels.com/photos/850216/pexels-photo-850216.jpeg?auto=compress&cs=tinysrgb&w=600">
+      <img src="https://cdn.pixabay.com/photo/2019/07/23/17/19/trash-4358272_640.jpg">
+      <img src="https://cdn.pixabay.com/photo/2020/04/16/15/23/containers-5051093_640.jpg">
+      <img src="https://images.pexels.com/photos/3457243/pexels-photo-3457243.jpeg?auto=compress&cs=tinysrgb&w=600">
     </div>
-    <div class="form" style="width: 30%">
+    <div class="form mx-auto pt-40" style="width: 25%">
       <form class="block mt-24"  @submit.prevent="sendEmail">
         <h1 class="text-5xl font-bold mb-6">{{ $t('contact-us.title') }}</h1>
         <p class="my-4">{{ $t('contact-us.text') }}</p>
@@ -94,19 +86,35 @@ export default {
             Message
           </label>
         </div>
-        <button type="submit" class="text-black mt-8 py-4 w-full bg-my-yellow ">{{ $t('contact-us.send') }}</button>
+        <button type="submit" class="text-black mt-8 py-4 w-full bg-my-yellow rounded">{{ $t('contact-us.send') }}</button>
       </form>
     </div>
-    <div class="bg-my-yellow w-52 flex justify-around align-end p-4">
-      <svg width="20" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z"/></svg>
-      <svg width="20" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M512 256C512 114.6 397.4 0 256 0S0 114.6 0 256C0 376 82.7 476.8 194.2 504.5V334.2H141.4V256h52.8V222.3c0-87.1 39.4-127.5 125-127.5c16.2 0 44.2 3.2 55.7 6.4V172c-6-.6-16.5-1-29.6-1c-42 0-58.2 15.9-58.2 57.2V256h83.6l-14.4 78.2H287V510.1C413.8 494.8 512 386.9 512 256h0z"/></svg>
-      <svg width="20" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M459.4 151.7c.3 4.5 .3 9.1 .3 13.6 0 138.7-105.6 298.6-298.6 298.6-59.5 0-114.7-17.2-161.1-47.1 8.4 1 16.6 1.3 25.3 1.3 49.1 0 94.2-16.6 130.3-44.8-46.1-1-84.8-31.2-98.1-72.8 6.5 1 13 1.6 19.8 1.6 9.4 0 18.8-1.3 27.6-3.6-48.1-9.7-84.1-52-84.1-103v-1.3c14 7.8 30.2 12.7 47.4 13.3-28.3-18.8-46.8-51-46.8-87.4 0-19.5 5.2-37.4 14.3-53 51.7 63.7 129.3 105.3 216.4 109.8-1.6-7.8-2.6-15.9-2.6-24 0-57.8 46.8-104.9 104.9-104.9 30.2 0 57.5 12.7 76.7 33.1 23.7-4.5 46.5-13.3 66.6-25.3-7.8 24.4-24.4 44.8-46.1 57.8 21.1-2.3 41.6-8.1 60.4-16.2-14.3 20.8-32.2 39.3-52.6 54.3z"/></svg>
+    <div class="block my-auto mr-8">
+      <a href="mailto:maria.spam2002@gmail.com">
+        <div class="p-4 rounded-xl" style="background-color: rgba(5,130,64,0.15)">
+            <svg width="15" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="#058240" d="M48 64C21.5 64 0 85.5 0 112c0 15.1 7.1 29.3 19.2 38.4L236.8 313.6c11.4 8.5 27 8.5 38.4 0L492.8 150.4c12.1-9.1 19.2-23.3 19.2-38.4c0-26.5-21.5-48-48-48H48zM0 176V384c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V176L294.4 339.2c-22.8 17.1-54 17.1-76.8 0L0 176z"/></svg>
+        </div>
+      </a>
+      <a href="tel:01602417136">
+        <div class="mt-4 p-4 rounded-xl" style="background-color: rgba(5,130,64,0.15)">
+            <svg width="15" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="#058240" d="M164.9 24.6c-7.7-18.6-28-28.5-47.4-23.2l-88 24C12.1 30.2 0 46 0 64C0 311.4 200.6 512 448 512c18 0 33.8-12.1 38.6-29.5l24-88c5.3-19.4-4.6-39.7-23.2-47.4l-96-40c-16.3-6.8-35.2-2.1-46.3 11.6L304.7 368C234.3 334.7 177.3 277.7 144 207.3L193.3 167c13.7-11.2 18.4-30 11.6-46.3l-40-96z"/></svg>
+        </div>
+      </a>
+      <a href="https://www.google.com/maps/place/Amberger+Str.+180,+93057+Regensburg/@49.048593,12.0988491,17z/data=!3m1!4b1!4m5!3m4!1s0x479fc1d15f1383f7:0xb5e53f3d9485f9d7!8m2!3d49.0485895!4d12.101424?entry=ttu">
+        <div class="mt-4 p-4 rounded-xl" style="background-color: rgba(5,130,64,0.15)">
+          <svg width="15" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512"><path fill="#058240" d="M288 48c0-26.5 21.5-48 48-48h96c26.5 0 48 21.5 48 48V192h40V120c0-13.3 10.7-24 24-24s24 10.7 24 24v72h24c26.5 0 48 21.5 48 48V464c0 26.5-21.5 48-48 48H432 336c-26.5 0-48-21.5-48-48V48zm64 32v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V80c0-8.8-7.2-16-16-16H368c-8.8 0-16 7.2-16 16zm16 80c-8.8 0-16 7.2-16 16v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V176c0-8.8-7.2-16-16-16H368zM352 272v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V272c0-8.8-7.2-16-16-16H368c-8.8 0-16 7.2-16 16zm176-16c-8.8 0-16 7.2-16 16v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V272c0-8.8-7.2-16-16-16H528zM512 368v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V368c0-8.8-7.2-16-16-16H528c-8.8 0-16 7.2-16 16zM224 160c0 6-1 11-2 16c20 14 34 38 34 64c0 45-36 80-80 80H160V480c0 18-15 32-32 32c-18 0-32-14-32-32V320H80c-45 0-80-35-80-80c0-26 13-50 33-64c-1-5-1-10-1-16c0-53 42-96 96-96c53 0 96 43 96 96z"/></svg>
+        </div>
+      </a>
     </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
   .contact {
+    .images {
+      display: grid;
+      grid-template-columns: 1fr 1fr;
+    }
     .body--dark & {
       background-color: #151515;
       color: white;

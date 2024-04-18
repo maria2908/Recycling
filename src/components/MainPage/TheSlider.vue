60a713c769fd8bae0f8d85db1dce6f4f78d8e8df
mahosobe <mariia.honchariuk@sobaco-betax.com>
2024-04-18 13:35:54 +0200

    main tablet

--- src/components/MainPage/TheSlider.vue
+++ src/components/MainPage/TheSlider.vue
@@ -6,15 +6,15 @@ import 'swiper/css/pagination';
 
 <template>
   <div class="main flex justify-around align-center">
-    <div style="width: 750px;">
-      <h1 class="text-my-dark-green font-black xl:text-9xl md:text-7xl">{{ $t('main-title')}}</h1>
+    <div class="text" style="width: 750px;">
+      <h1 class="text-9xl text-my-dark-green font-black">{{ $t('main-title')}}</h1>
       <p class="my-12 font-semibold">{{ $t('main-text')}}</p>
       <router-link :to="{}" class="p-4 rounded border-my-dark-green border-2 text-my-dark-green hover:border-my-green hover:text-my-green">{{ $t('read-more')}}</router-link>
       <router-link :to="{}" class="bg-my-dark-green hover:bg-my-green border-my-dark-green hover:border-my-green border-2 text-white ml-4 p-4 rounded px-7 text-center">{{ $t('read-news')}}</router-link>
     </div>
     <div class="flex slider">
       <swiper
-          class="mr-12 pt-16"
+          class="slide mr-12 pt-16"
           style="height: 100vh; width: 250px"
           :loop="true"
           :space-between="0"
@@ -30,7 +30,7 @@ import 'swiper/css/pagination';
         <swiper-slide class="my-6"><img src="https://images.pexels.com/photos/6990304/pexels-photo-6990304.jpeg?auto=compress&cs=tinysrgb&w=600" /></swiper-slide>
       </swiper>
       <swiper
-          class="mr-12"
+          class="slide mr-12"
           style="height: 100vh; width: 250px"
           :loop="true"
           :space-between="0"
@@ -40,13 +40,12 @@ import 'swiper/css/pagination';
         <swiper-slide class="my-2"><img src="https://images.pexels.com/photos/3010250/pexels-photo-3010250.jpeg?auto=compress&cs=tinysrgb&w=600" /></swiper-slide>
         <swiper-slide class="my-2"><img src="https://images.pexels.com/photos/4167579/pexels-photo-4167579.jpeg?auto=compress&cs=tinysrgb&w=600" /></swiper-slide>
         <swiper-slide class="my-2"><img src="https://images.pexels.com/photos/2927582/pexels-photo-2927582.jpeg?auto=compress&cs=tinysrgb&w=600" /></swiper-slide>
-        <swiper-slide class="my-2"><img src="https://images.pexels.com/photos/2927582/pexels-photo-2927582.jpeg?auto=compress&cs=tinysrgb&w=600" /></swiper-slide>
         <swiper-slide class="my-2"><img src="https://images.pexels.com/photos/7772006/pexels-photo-7772006.jpeg?auto=compress&cs=tinysrgb&w=600" /></swiper-slide>
         <swiper-slide class="my-2"><img src="https://images.pexels.com/photos/12033659/pexels-photo-12033659.jpeg?auto=compress&cs=tinysrgb&w=600" /></swiper-slide>
       </swiper>
       <swiper
           style="height: 100vh; width: 250px"
-          class="pt-16"
+          class="slide pt-16"
           :loop="true"
           :space-between="0"
           :direction="'vertical'"
@@ -63,12 +62,33 @@ import 'swiper/css/pagination';
   </div>
 </template>
 
-<style>
-@media (min-width: 1440px) {
-  slider {
-    img {
-      width: 100px;
+<style scoped lang="scss">
+@media (max-width: 1440px) {
+    .main {
+      width: 100%;
+      .text {
+        padding: 15px;
+        width: 45%;
+        h1 {
+          font-size: 70px !important;
+        }
+        p {
+          font-size: 14px;
+        }
+        a {
+          padding: 15px;
+        }
+      }
+
+
+      .slider {
+        width: 55% !important;
+
+        .swiper-slide {
+          height: 30vh !important;
+        }
+      }
+
     }
-  }
 }
 </style>
